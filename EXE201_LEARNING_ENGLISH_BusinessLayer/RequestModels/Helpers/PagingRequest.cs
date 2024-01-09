d3da5243dcba27a4f436503440923ad902bbd99c
nea287 <thonguyenngockieu@gmail.com>
2024-01-09 16:27:03 +0700

    build/addition-models

---
+++ EXE201_LEARNING_ENGLISH_BusinessLayer/RequestModels/Helpers/PagingRequest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXE201_LEARNING_ENGLISH_BusinessLayer.RequestModels.Helpers
+{
+    public class PagingRequest
+    {
+        public int page { get; set; } = 1;
+        public int pageSize { get; set; } = 10;
+        public string KeySearch { get; set; } = "";
+        public string SearchBy { get; set; } = "";
+        public SortOrder SortType { get; set; } = SortOrder.Descending;
+        public string CollName { get; set; } = "Id";
+    }
+}
