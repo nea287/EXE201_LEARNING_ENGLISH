a456f28402c7da897792b5265f1ce780a0e8d0b5
nea287 <thonguyenngockieu@gmail.com>
2024-01-09 19:09:50 +0700

    build/buid-5

--- EXE201_LEARNING_ENGLISH_BusinessLayer/Services/AccountService.cs
+++ EXE201_LEARNING_ENGLISH_BusinessLayer/Services/AccountService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using EXE201_LEARNING_ENGLISH_BusinessLayer.Common;
 using EXE201_LEARNING_ENGLISH_BusinessLayer.FilterModels;
+using EXE201_LEARNING_ENGLISH_BusinessLayer.Helpers;
 using EXE201_LEARNING_ENGLISH_BusinessLayer.IServices;
 using EXE201_LEARNING_ENGLISH_BusinessLayer.ReponseModels;
 using EXE201_LEARNING_ENGLISH_BusinessLayer.ReponseModels.Heplers;
@@ -13,6 +15,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace EXE201_LEARNING_ENGLISH_BusinessLayer.Services
@@ -31,19 +34,39 @@ namespace EXE201_LEARNING_ENGLISH_BusinessLayer.Services
         {
             try
             {
+                //Validate
+                if (!PhoneNumberValidate(request.PhoneNumber))
+                {
+                    return new ResponseResult<AccountReponse>()
+                    {
+                        Message = Constraints.NUMBER_PHONE_VALIDATE,
+                        result = false
+
+                    };
+                }
+
                 var existedAccount = _repository.GetByIdByString(request.Email).Result;
                 if(existedAccount != null)
                 {
                     return new ResponseResult<AccountReponse>()
                     {
                         Message = Constraints.EXISTED_INFO,
+                        result = false
                         
                     };
                 }
+
+                _repository.Insert(_mapper.Map<Account>(request));
+                _repository.Save();
             }
             catch (Exception ex)
             {
+                return new ResponseResult<AccountReponse>()
+                {
+                    Message = Constraints.CREATE_INFO_FAILED,
+                    result = false
 
+                };
             }
             finally
             {
@@ -52,8 +75,8 @@ namespace EXE201_LEARNING_ENGLISH_BusinessLayer.Services
 
             return new ResponseResult<AccountReponse>()
             {
-                Message = Constraints.EXISTED_INFO,
-
+                Message = Constraints.CREATE_INFO_SUCCESS,
+                result = true
             };
             
 
@@ -61,7 +84,41 @@ namespace EXE201_LEARNING_ENGLISH_BusinessLayer.Services
 
         public ResponseResult<AccountReponse> DeleteAccount(string email)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var existedAccount = _repository.GetByIdByString(email).Result;
+
+                if(existedAccount == null)
+                {
+                    return new ResponseResult<AccountReponse>()
+                    {
+                        Message = Constraints.NOT_FOUND_INFO,
+                        result = false,
+                    };
+                }
+
+                existedAccount.Status = 0;
+                _repository.UpdateByIdByString(existedAccount, email);
+                _repository.Save();
+
+            }catch(Exception ex)
+            {
+                return new ResponseResult<AccountReponse>()
+                {
+                    Message = Constraints.DELELTE_INFO_FAILED,
+                    result = false,
+                };
+            }
+            finally
+            {
+                lock (_repository) ;
+            }
+
+            return new ResponseResult<AccountReponse>()
+            {
+                Message = Constraints.DELETE_INFO_SUCCESS,
+                result = true,
+            };
         }
 
         public ResponseResult<AccountReponse> GetAccount(string email)
@@ -71,12 +128,28 @@ namespace EXE201_LEARNING_ENGLISH_BusinessLayer.Services
 
         public DynamicModelResponse.DynamicModelsResponse<AccountReponse> GetAccounts(AccountFilter request, PagingRequest paging)
         {
-            throw new NotImplementedException();
+            (int, IQueryable<AccountReponse>) result;
+            try
+            {
+                //result = _repository.GetAll().ProjectTo<AccountReponse>(_mapper.ConfigurationProvider)
+                //    .DynamicFilter(_mapper.Map<AccountReponse>(request))
+                //    .Pa
+            }catch(Exception ex)
+            {
+
+            }
         }
 
         public ResponseResult<AccountReponse> UpdateAccount(UpdateAccountRequest request)
         {
             throw new NotImplementedException();
         }
+
+        //Validate 
+        //Validate Phone Number
+        public bool PhoneNumberValidate(string phoneNumber)
+            => Regex.IsMatch(phoneNumber, @"[^0-9]") 
+            && Regex.IsMatch(phoneNumber, @"^\d{10}$");
+        
     }
 }
